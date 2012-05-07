7babe5443615b24532af2f8390383eae5eb72258
Ariel Núñez <ingenieroariel@gmail.com>
2012-05-07 15:43:18 -0400

    Added default urls.py

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,6 +1,51 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls.defaults import *
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from geonode.sitemap import LayerSitemap, MapSitemap
+import geonode.proxy.urls
 
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns("",
-) + staticfiles_urlpatterns()
+js_info_dict = {
+    'domain': 'djangojs',
+    'packages': ('geonode',)
+}
+
+sitemaps = {
+    "layer": LayerSitemap,
+    "map": MapSitemap
+}
+
+urlpatterns = patterns('',
+    # Static pages
+    url(r'^index/$', 'geonode.views.index', name='home'),
+    url(r'^help/$', 'geonode.views.help', name='help'),
+    url(r'^developer/$', 'geonode.views.developer', name='dev'),
+
+    # Data views
+    (r'^data/', include('geonode.maps.urls.datapatterns')),
+    (r'^maps/', include('geonode.maps.urls.urlpatterns')),
+
+    (r'^comments/', include('dialogos.urls')),
+    (r'^ratings/', include('agon_ratings.urls')),
+
+    # Accounts
+    url(r'^accounts/ajax_login$', 'geonode.views.ajax_login', name='auth_ajax_login'),
+    url(r'^accounts/ajax_lookup$', 'geonode.views.ajax_lookup', name='auth_ajax_lookup'),
+    (r'^accounts/', include('registration.urls')),
+    (r'^profiles/', include('profiles.urls')),
+    (r'^avatar/', include('avatar.urls')),
+
+    # Meta
+    url(r'^lang\.js$', 'django.views.generic.simple.direct_to_template',
+               {'template': 'lang.js', 'mimetype': 'text/javascript'}, name='lang'),
+    (r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict, name='jscat'),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}, name='sitemap'),
+    (r'^admin/', include(admin.site.urls)),
+    )
+
+urlpatterns += geonode.proxy.urls.urlpatterns
+
+# Serve static files if a proxy is not configured
+urlpatterns += staticfiles_urlpatterns()
