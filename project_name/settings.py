6f5617c0c623eda678b6956cfa1f3ed6e6cf185b
Ariel Núñez <ingenieroariel@gmail.com>
2012-05-07 15:37:50 -0400

    Added missing GeoNode settings

--- project_name/settings.py
+++ project_name/settings.py
@@ -1,7 +1,9 @@
 import os
+import geonode
 
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+GEONODE_ROOT = os.path.dirname(geonode.__file__)
 
 SECRET_KEY = "{{ secret_key }}"
 
@@ -17,7 +19,7 @@ TIME_ZONE = "America/Chicago"
 LANGUAGE_CODE = "en-us"
 
 # These are for user-uploaded content.
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "site_media")
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
 MEDIA_URL = "/media/"
 
 # These are for site static media (e.g. CSS and JS)
@@ -27,6 +29,7 @@ STATIC_URL = "/static/"
 ADMIN_MEDIA_PREFIX = "/static/admin/"
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, "static"),
+    os.path.join(GEONODE_ROOT, "media"),
 ]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
@@ -81,3 +84,151 @@ INSTALLED_APPS = [
     "django.contrib.auth",
     "django.contrib.contenttypes",
 ]
+#
+# GeoNode specific settings
+#
+
+# Setting a custom test runner to avoid running the tests for some problematic 3rd party apps
+TEST_RUNNER='geonode.testrunner.GeoNodeTestRunner'
+
+# Needed to override serving of Javascript files
+GEONODE_CLIENT_LOCATION = STATIC_URL + "geonode/"
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "geonode.maps.context_processors.resource_urls",
+)
+
+
+# The FULLY QUALIFIED url to the GeoServer instance for this GeoNode.
+GEOSERVER_BASE_URL = "http://localhost:8001/geoserver-geonode-dev/"
+
+# The username and password for a user that can add and edit layer details on GeoServer
+GEOSERVER_CREDENTIALS = "geoserver_admin", SECRET_KEY
+
+# The FULLY QUALIFIED url to the GeoNetwork instance for this GeoNode
+GEONETWORK_BASE_URL = "http://localhost:8001/geonetwork/"
+
+# The username and password for a user with write access to GeoNetwork
+GEONETWORK_CREDENTIALS = "admin", "admin"
+
+AUTHENTICATION_BACKENDS = ('geonode.core.auth.GranularBackend',)
+
+# Where should newly created maps be focused?
+DEFAULT_MAP_CENTER = (0, 0)
+
+# How tightly zoomed should newly created maps be?
+# 0 = entire world;
+# maximum zoom is between 12 and 15 (for Google Maps, coverage varies by area)
+DEFAULT_MAP_ZOOM = 0
+
+MAP_BASELAYERS = [{
+    "source": {"ptype": "gx_olsource"},
+    "type":"OpenLayers.Layer",
+    "args":["No background"],
+    "visibility": False,
+    "fixed": True,
+    "group":"background"
+  },{
+    "source": { "ptype":"gx_olsource"},
+    "type":"OpenLayers.Layer.OSM",
+    "args":["OpenStreetMap"],
+    "visibility": True,
+    "fixed": True,
+    "group":"background"
+  },{
+    "source": {"ptype":"gx_olsource"},
+    "type":"OpenLayers.Layer.WMS",
+    "group":"background",
+    "visibility": False,
+    "fixed": True,
+    "args":[
+      "bluemarble",
+      "http://maps.opengeo.org/geowebcache/service/wms",
+      {
+        "layers":["bluemarble"],
+        "format":"image/png",
+        "tiled": True,
+        "tilesOrigin":[-20037508.34,-20037508.34]
+      },
+      {"buffer":0}
+    ]
+
+}]
+
+
+def get_user_url(u):
+    from django.contrib.sites.models import Site
+    s = Site.objects.get_current()
+    return "http://" + s.domain + "/profiles/" + u.username
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'file':{
+            'level':'DEBUG',
+            'class' : 'logging.handlers.RotatingFileHandler',
+            'formatter': 'verbose',
+            'filename': 'yemendata.log',
+            'maxBytes': '1024',
+            'backupCount': '3',
+         },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['null'],
+            'propagate': True,
+            'level':'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'geonode': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    }
+}
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': get_user_url
+}
+
+AUTH_PROFILE_MODULE = 'maps.Contact'
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+
+DB_DATASTORE = True
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
