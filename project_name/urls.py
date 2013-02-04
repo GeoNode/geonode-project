99c1328e2ccfca8034fe37911b49aee63cc00d04
Jeffrey Johnson <jjohnson@opengeo.org>
2013-02-04 11:13:03 -0800

    Update settings.py and urls.py to match current master

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,9 +1,14 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, patterns, url
+from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 from geonode.sitemap import LayerSitemap, MapSitemap
 import geonode.proxy.urls
-import geonode.maps.urls
 
+# Import *_signals.py
+import geonode.social_signals
+
+# Setup Django Admin
 from django.contrib import admin
 admin.autodiscover()
 
@@ -18,35 +23,70 @@ sitemaps = {
 }
 
 urlpatterns = patterns('',
+
     # Static pages
-    url(r'^$', 'geonode.views.index', name='home'),
-    url(r'^help/$', 'geonode.views.help', name='help'),
-    url(r'^developer/$', 'geonode.views.developer', name='dev'),
+    url(r'^$', 'django.views.generic.simple.direct_to_template',
+                {'template': 'index.html'}, name='home'),
+    url(r'^help/$', 'django.views.generic.simple.direct_to_template',
+                {'template': 'help.html'}, name='help'),
+    url(r'^developer/$', 'django.views.generic.simple.direct_to_template',
+                {'template': 'developer.html'}, name='developer'),
+    url(r'^about/$', 'django.views.generic.simple.direct_to_template',
+                {'template': 'about.html'}, name='about'),
 
-    # Data views
-    (r'^data/', include(geonode.maps.urls.datapatterns)),
-    (r'^maps/', include(geonode.maps.urls.urlpatterns)),
+    # Layer views
+    (r'^layers/', include('geonode.layers.urls')),
 
+    # Map views
+    (r'^maps/', include('geonode.maps.urls')),
+
+    # Catalogue views
+    (r'^catalogue/', include('geonode.catalogue.urls')),
+
+    # Search views
+    (r'^search/', include('geonode.search.urls')),
+
+    # Upload views
+    (r'^upload/', include('geonode.upload.urls')),
+
+    # Social views
+    (r"^account/", include("account.urls")),
+    (r'^people/', include('geonode.people.urls')),
+    (r'^avatar/', include('avatar.urls')),
     (r'^comments/', include('dialogos.urls')),
     (r'^ratings/', include('agon_ratings.urls')),
+    (r'^activity/', include('actstream.urls')),
+    (r'^relationships/', include('relationships.urls')),
+    (r'^announcements/', include('announcements.urls')),
+    #(r'^notifications/', include('notification.urls')),
+    (r'^messages/', include('user_messages.urls')),
 
     # Accounts
-    url(r'^accounts/ajax_login$', 'geonode.views.ajax_login', name='auth_ajax_login'),
-    url(r'^accounts/ajax_lookup$', 'geonode.views.ajax_lookup', name='auth_ajax_lookup'),
-    (r'^accounts/', include('registration.urls')),
-    (r'^profiles/', include('profiles.urls')),
-    (r'^avatar/', include('avatar.urls')),
+    url(r'^account/ajax_login$', 'geonode.views.ajax_login',
+                                       name='account_ajax_login'),
+    url(r'^account/ajax_lookup$', 'geonode.views.ajax_lookup',
+                                       name='account_ajax_lookup'),
 
     # Meta
     url(r'^lang\.js$', 'django.views.generic.simple.direct_to_template',
-               {'template': 'lang.js', 'mimetype': 'text/javascript'}, name='lang'),
+         {'template': 'lang.js', 'mimetype': 'text/javascript'}, name='lang'),
+    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog',
+                                  js_info_dict, name='jscat'),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap',
+                                  {'sitemaps': sitemaps}, name='sitemap'),
     (r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict, name='jscat'),
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}, name='sitemap'),
     (r'^admin/', include(admin.site.urls)),
+
+    )
+
+#Documents views
+if settings.DOCUMENTS_APP:
+    urlpatterns += patterns('',
+        (r'^documents/', include('geonode.documents.urls')),
     )
 
 urlpatterns += geonode.proxy.urls.urlpatterns
 
-# Serve static files if a proxy is not configured
+# Serve static files
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
