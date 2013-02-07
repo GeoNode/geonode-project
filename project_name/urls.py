a43384376d18877fa0844618308f093fd4870f03
Jeffrey Johnson <jjohnson@opengeo.org>
2013-02-07 10:46:39 -0800

    Use the right template for the homepage

--- project_name/urls.py
+++ project_name/urls.py
@@ -26,7 +26,7 @@ urlpatterns = patterns('',
 
     # Static pages
     url(r'^$', 'django.views.generic.simple.direct_to_template',
-                {'template': 'index.html'}, name='home'),
+                {'template': 'site_index.html'}, name='home'),
     url(r'^help/$', 'django.views.generic.simple.direct_to_template',
                 {'template': 'help.html'}, name='help'),
     url(r'^developer/$', 'django.views.generic.simple.direct_to_template',
