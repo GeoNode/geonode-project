657a8c8d8bd217e1e0074123ce22a3aeed3d6ee0
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2017-06-27 16:15:10 +0200

    Make use of new updates "pinax.notifications" module
    We need to remap root URLs accordingly, otherwise it won't work on customized projects.
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures
    
     - Fix geonode project dependencies and fixtures

--- project_name/__init__.py
+++ project_name/__init__.py
@@ -1,20 +1,19 @@
-import os
-
-__version__ = (2, 5, 7, 'alpha', 0)
-
-
-class GeoNodeException(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-
-def get_version():
-    import {{ project_name }}.version
-    return {{ project_name }}.version.get_version(__version__)
-
-
-def main(global_settings, **settings):
-    from django.core.wsgi import get_wsgi_application
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings.get('django_settings'))
-    app = get_wsgi_application()
-    return app
+# -*- coding: utf-8 -*-
+#########################################################################
+#
+# Copyright (C) 2017 OSGeo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#########################################################################
