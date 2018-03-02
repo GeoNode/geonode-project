2317a7f2323f29c814b20b7c791c8b50ca1dc08d
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-03-02 19:21:34 +0100

    - fix settings.py

--- project_name/settings.py
+++ project_name/settings.py
@@ -59,7 +59,7 @@ MANAGERS = ADMINS = os.getenv('ADMINS', [])
 TIME_ZONE = os.getenv('TIME_ZONE', "America/Chicago")
 USE_TZ = True
 
-INSTALLED_APPS += (PROJECT_NAME,)
+INSTALLED_APPS += ('geonode', PROJECT_NAME,)
 
 # Location of url mappings
 ROOT_URLCONF = os.getenv('ROOT_URLCONF', '{}.urls'.format(PROJECT_NAME))
