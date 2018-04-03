5d040e99cf354a9eeb1bd99de79b4e948bf3b3e7
Alessio Fabiani <alessio.fabiani@geo-solutions.it>
2018-04-03 13:15:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #########################################################################
 #
-# Copyright (C) 2017 OSGeo
+# Copyright (C) 2018 OSGeo
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,11 +25,11 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
-    name="{{ project_name }}",
+    name="my_geonode",
     version="0.1",
     author="",
     author_email="",
-    description="{{ project_name }}, based on GeoNode",
+    description="my_geonode, based on GeoNode",
     long_description=(read('README.rst')),
     # Full list of classifiers can be found at:
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -37,9 +37,9 @@ setup(
         'Development Status :: 1 - Planning',
     ],
     license="BSD",
-    keywords="{{ project_name }} geonode django",
-    url='https://github.com/{{ project_name }}/{{ project_name }}',
-    packages=['{{ project_name }}',],
+    keywords="my_geonode geonode django",
+    url='https://github.com/my_geonode/my_geonode',
+    packages=['my_geonode',],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
@@ -47,13 +47,17 @@ setup(
        'six==1.10.0',
        'django-cuser==2017.3.16',
        'django-model-utils==3.1.1',
+       'django-autocomplete-light==2.3.3',
        'pyshp==1.2.12',
        'celery==4.1.0',
        'Shapely>=1.5.13,<1.6.dev0',
+       'OWSLib==0.15.0',
        'proj==0.1.0',
        'pyproj==1.9.5.1',
-       'pygdal==2.2.1.3',
-       'inflection==0.3.1'
-       'geonode==2.8.0'
+       'inflection==0.3.1',
+       'oauthlib==2.0.1',
+       'python-dateutil==2.6.1',
+       'pycsw==2.0.3',
+       'geonode==2.8',
     ],
 )
