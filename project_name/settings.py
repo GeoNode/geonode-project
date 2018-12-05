70feefacb5bef0830a28309ab5a82f0b8904f4e8
afabiani <alessio.fabiani@geo-solutions.it>
2018-12-05 19:04:49 +0100

    - fix site host name and port

--- project_name/settings.py
+++ project_name/settings.py
@@ -46,7 +46,6 @@ LOCAL_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 WSGI_APPLICATION = "{}.wsgi.application".format(PROJECT_NAME)
 
-
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', "en")
