a099c7f22e6ff031599dac98cfa52e42146dbc91
afabiani <alessio.fabiani@geo-solutions.it>
2020-08-31 11:56:25 +0200

    [ref. Geonode/geonode#6388] Error when installing core GeoNode into virtual environment

--- setup.py
+++ setup.py
@@ -17,41 +17,19 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
-
 import os
-try:
-    # pip >=20
-    from pip._internal.network.session import PipSession
-    try:
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip >=21
-        from pip._internal.req.req_file import parse_requirements
-except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip <= 9.0.3
-        from pip.download import PipSession
-        from pip.req import parse_requirements
 
 from distutils.core import setup
 
 from setuptools import find_packages
 
-# Parse requirements.txt to get the list of dependencies
-inst_req = parse_requirements("requirements.txt", session=PipSession())
-REQUIREMENTS = [str(r.req) if hasattr(r, 'req') else r.requirement if not r.is_editable else ''
-                for r in inst_req]
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
     name="{{ project_name }}",
-    version="3.0",
+    version="3.1",
     author="",
     author_email="",
     description="{{ project_name }}, based on GeoNode",
@@ -65,10 +43,8 @@ setup(
     keywords="{{ project_name }} geonode django",
     url='https://github.com/{{ project_name }}/{{ project_name }}',
     packages=find_packages(),
-    install_requires=REQUIREMENTS,
     dependency_links=[
         "git+https://github.com/GeoNode/geonode.git#egg=geonode"
     ],
     include_package_data=True,
-    zip_safe=False,
 )
