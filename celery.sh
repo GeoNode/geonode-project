454b3d3d687828b5ce301fa91b09e95c00704c83
afabiani <alessio.fabiani@geo-solutions.it>
2020-10-27 10:59:04 +0100

    Improve Celery Async Tasks configuration
    (cherry picked from commit d20d2004207219daf9f5ff2b18ee1722a46d3c8f)

--- celery.sh
+++ celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-celery -A geonode.celery_app:app beat -l DEBUG -f /var/log/celery.log &
-celery -A geonode.celery_app:app worker -B -E -Q broadcast,default,geonode,update,cleanup,email,email.events,notifications.events,all.geoserver,geoserver.catalog,geoserver.data,geoserver.events,geonode.layer.viewer -l DEBUG -c 4 -P threads -f /var/log/celery.log &
-celery -A geonode.celery_app:app flower --auto_refresh=True --debug=False --broker=${BROKER_URL} --basic_auth=${ADMIN_USERNAME}:${ADMIN_PASSWORD} --address=0.0.0.0 --port=5555 &
+nohup celery -A geonode.celery_app:app beat -l DEBUG -f /var/log/celery.log &>/dev/null &
+nohup celery -A geonode.celery_app:app worker -B -E -Q broadcast,default,geonode,update,cleanup,email,email.events,notifications.events,all.geoserver,geoserver.catalog,geoserver.data,geoserver.events,geonode.layer.viewer -l DEBUG -c 4 -P threads -f /var/log/celery.log &>/dev/null &
+nohup celery -A geonode.celery_app:app flower --auto_refresh=True --debug=False --broker=${BROKER_URL} --basic_auth=${ADMIN_USERNAME}:${ADMIN_PASSWORD} --address=0.0.0.0 --port=5555 &>/dev/null &
