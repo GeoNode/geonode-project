18ff1b611c3a119ab70b2229497b17a23c666ce3
afabiani <alessio.fabiani@geo-solutions.it>
2020-11-12 15:53:16 +0100

    - Use env variable to connect to the default DB instead of hardcoded values

--- wait-for-databases.sh
+++ wait-for-databases.sh
@@ -5,7 +5,7 @@ set -e
 host="$1"
 shift
 
-until PGPASSWORD=postgres psql -h "$host" -U "postgres" -P "pager=off" -c '\l'; do
+until PGPASSWORD=${POSTGRES_PASSWORD} psql -h "$host" -U ${POSTGRES_USER} -P "pager=off" -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
