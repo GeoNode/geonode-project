4a2bff99f28338b8c19eb3e2a0795b6e686a1fc9
giohappy <giohappy@gmail.com>
2021-03-05 15:42:56 +0100

    let the project override geonode root templates

--- project_name/apps.py
+++ project_name/apps.py
@@ -17,15 +17,18 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 #########################################################################
+import os
 from django.apps import AppConfig as BaseAppConfig
-
 
 def run_setup_hooks(*args, **kwargs):
     from django.conf import settings
     from .celeryapp import app as celeryapp
-    if celeryapp not in settings.INSTALLED_APPS:
-        settings.INSTALLED_APPS += (celeryapp, )
 
+    LOCAL_ROOT = os.path.abspath(os.path.dirname(__file__))
+    settings.TEMPLATES[0]["DIRS"].insert(0, os.path.join(LOCAL_ROOT, "templates"))
+
+    if celeryapp not in settings.INSTALLED_APPS:
+        settings.INSTALLED_APPS += (celeryapp,)
 
 class AppConfig(BaseAppConfig):
 
