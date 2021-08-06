76d237ad39062ae6e01f14c6e51921b33caa1b3f
afabiani <alessio.fabiani@geo-solutions.it>
2021-08-06 16:31:06 +0200

    [Fixes #207] unknown command: 'set_all_layers_alternate' (master branch)

--- src/pavement.py
+++ src/pavement.py
@@ -431,7 +431,7 @@ def sync(options):
     sh("%s python -W ignore manage.py loaddata fixtures/initial_data.json" % settings)
     if 'django_celery_beat' in INSTALLED_APPS:
         sh("%s python -W ignore manage.py loaddata fixtures/django_celery_beat.json" % settings)
-    sh("%s python -W ignore manage.py set_all_layers_alternate" % settings)
+    sh("%s python -W ignore manage.py set_all_datasets_alternate" % settings)
 
 
 @task
--- src/tasks.py
+++ src/tasks.py
@@ -240,9 +240,9 @@ def fixtures(ctx):
 --settings={0}".format(_localsettings()), pty=True)
     ctx.run("python manage.py loaddata /usr/src/{{project_name}}/fixtures/initial_data.json \
 --settings={0}".format(_localsettings()), pty=True)
-    ctx.run("python manage.py set_all_layers_alternate \
+    ctx.run("python manage.py set_all_datasets_alternate \
 --settings={0}".format(_localsettings()), pty=True)
-#     ctx.run("python manage.py set_all_layers_metadata -d \
+#     ctx.run("python manage.py set_all_datasets_metadata -d \
 # --settings={0}".format(_localsettings()), pty=True)
 
 
