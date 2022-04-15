e2464d10fb06829843d74e44b078461a08f3c519
Luca Pasquali <luca@bucaniere.us>
2022-04-15 18:12:10 +0200

    update vagrant scripts to make geonode with configured oauth and its documentation in README.md

--- antani-vagrant-compose.sh
+++ antani-vagrant-compose.sh
@@ -9,6 +9,13 @@ rm -rf antani
 django-admin startproject --template=/home/vagrant/geonode-project -e py,sh,md,rst,json,yml,ini,env,sample,properties -n monitoring-cron -n Dockerfile antani
 cd /home/vagrant/antani
 cp .env.sample .env
+sed -i 's/GEOSERVER_WEB_UI_LOCATION=http:\/\/localhost\/geoserver\//GEOSERVER_WEB_UI_LOCATION=http:\/\/localhost:8888\/geoserver\//' .env
+sed -i 's/GEOSERVER_PUBLIC_LOCATION=http:\/\/localhost\/geoserver\//GEOSERVER_PUBLIC_LOCATION=http:\/\/localhost:8888\/geoserver\//' .env
+sed -i 's/SITEURL=.*/SITEURL=http:\/\/localhost:8888\//' .env
+sed -i 's/GEONODE_LB_PORT=80/GEONODE_LB_PORT=8888/' .env
+sed -i "s/OAUTH2_CLIENT_ID=.*/OAUTH2_CLIENT_ID=$(pwgen 30 -1)/" .env
+sed -i "s/OAUTH2_CLIENT_SECRET=.*/OAUTH2_CLIENT_SECRET=$(pwgen 80 -1)/" .env
+sed -i "s/SECRET_KEY=.*/SECRET_KEY=$(pwgen 50 -1)/" .env
 chown -R vagrant:vagrant /home/vagrant/antani
 sudo docker-compose build
 sudo docker-compose up -d
