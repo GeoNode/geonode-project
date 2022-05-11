6a71b359d964e92df923844b67db2bc2f04cab29
giohappy <giohappy@gmail.com>
2022-05-11 21:35:41 +0200

    use create-env script to generate .env file with vagrant

--- testproject-vagrant-compose.sh
+++ testproject-vagrant-compose.sh
@@ -8,14 +8,11 @@ pip install Django==3.2
 rm -rf testproject
 django-admin startproject --template=/home/vagrant/geonode-project -e py,sh,md,rst,json,yml,ini,env,sample,properties -n monitoring-cron -n Dockerfile testproject
 cd /home/vagrant/testproject
-cp .env.sample .env
+python create-envfile.py
 sed -i 's/GEOSERVER_WEB_UI_LOCATION=http:\/\/localhost\/geoserver\//GEOSERVER_WEB_UI_LOCATION=http:\/\/localhost:8888\/geoserver\//' .env
 sed -i 's/GEOSERVER_PUBLIC_LOCATION=http:\/\/localhost\/geoserver\//GEOSERVER_PUBLIC_LOCATION=http:\/\/localhost:8888\/geoserver\//' .env
 sed -i 's/SITEURL=.*/SITEURL=http:\/\/localhost:8888\//' .env
 sed -i 's/GEONODE_LB_PORT=80/GEONODE_LB_PORT=8888/' .env
-sed -i "s/OAUTH2_CLIENT_ID=.*/OAUTH2_CLIENT_ID=$(pwgen 30 -1)/" .env
-sed -i "s/OAUTH2_CLIENT_SECRET=.*/OAUTH2_CLIENT_SECRET=$(pwgen 80 -1)/" .env
-sed -i "s/SECRET_KEY=.*/SECRET_KEY=$(pwgen 50 -1)/" .env
 chown -R vagrant:vagrant /home/vagrant/testproject
 sudo docker-compose build
 sudo docker-compose up -d
