dd8087ddcab33ef9d494caa6527b587c22cb0793
Giovanni Allegri <giohappy@gmail.com>
2022-05-12 10:20:24 +0200

    rename vagrant project (#316)
    * rename vagrant project
    
    * renamed generated project
    
    (cherry picked from commit 236bf090bbfea2f10861925ef31c2db19b45f7ad)

--- generated-project-vagrant-swarm.sh
+++ generated-project-vagrant-swarm.sh
@@ -1,13 +1,13 @@
 
 #!/usr/bin/env bash
-# this is the antani script to create an antani canary project.
-
+# this is the generated-project script to create an generated-project canary project.
+sudo docker swarm init
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-mkvirtualenv --python=/usr/bin/python3 antani
+mkvirtualenv --python=/usr/bin/python3 generated-project
 pip install Django==3.2
-rm -rf antani
-django-admin startproject --template=/home/vagrant/geonode-project -e py,sh,md,rst,json,yml,ini,env,sample,properties -n monitoring-cron -n Dockerfile antani
-cd /home/vagrant/antani
+rm -rf generated-project
+django-admin startproject --template=/home/vagrant/geonode-project -e py,sh,md,rst,json,yml,ini,env,sample,properties -n monitoring-cron -n Dockerfile generated-project
+cd /home/vagrant/generated-project
 cp .env.sample .env
 sed -i 's/GEOSERVER_WEB_UI_LOCATION=http:\/\/localhost\/geoserver\//GEOSERVER_WEB_UI_LOCATION=http:\/\/localhost:8888\/geoserver\//' .env
 sed -i 's/GEOSERVER_PUBLIC_LOCATION=http:\/\/localhost\/geoserver\//GEOSERVER_PUBLIC_LOCATION=http:\/\/localhost:8888\/geoserver\//' .env
@@ -16,6 +16,6 @@ sed -i 's/GEONODE_LB_PORT=80/GEONODE_LB_PORT=8888/' .env
 sed -i "s/OAUTH2_CLIENT_ID=.*/OAUTH2_CLIENT_ID=$(pwgen 30 -1)/" .env
 sed -i "s/OAUTH2_CLIENT_SECRET=.*/OAUTH2_CLIENT_SECRET=$(pwgen 80 -1)/" .env
 sed -i "s/SECRET_KEY=.*/SECRET_KEY=$(pwgen 50 -1)/" .env
-chown -R vagrant:vagrant /home/vagrant/antani
-sudo docker-compose build
-sudo docker-compose up -d
+chown -R vagrant:vagrant /home/vagrant/generated-project
+sudo docker-compose -f ./geonode-stack.yml build
+sudo docker stack deploy -c ./geonode-stack.yml geonode-stack
