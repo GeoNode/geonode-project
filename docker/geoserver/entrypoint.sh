f550e453a2daf1b45119097445843fdf9bfc696c
Alessio Fabiani <alessio.fabiani@geosolutionsgroup.com>
2022-12-01 09:56:41 +0100

    [Fixes #389] Allow "Enable CORS" within GeoServer container through env (#390)
    * [Fixes #389] Allow "Enable CORS" within GeoServer container through env
    
    * - Renaming the GEOSERVER CORS variables and place them on the sample envs
    
    * - Renaming the GEOSERVER CORS variables and place them on the sample envs
    
    (cherry picked from commit bfb9b4b09bd7af200468980c214cad364696d76a)

--- docker/geoserver/entrypoint.sh
+++ docker/geoserver/entrypoint.sh
@@ -115,8 +115,38 @@ for template in in ${geoserver_datadir_template_dirs[*]}; do
       done
 
     fi
-
 done
+
+# configure CORS (inspired by https://github.com/oscarfonts/docker-geoserver)
+# if enabled, this will add the filter definitions
+# to the end of the web.xml
+# (this will only happen if our filter has not yet been added before)
+if [ "${GEOSERVER_CORS_ENABLED}" = "true" ] || [ "${GEOSERVER_CORS_ENABLED}" = "True" ]; then
+  if ! grep -q DockerGeoServerCorsFilter "$CATALINA_HOME/webapps/geoserver/WEB-INF/web.xml"; then
+    echo "Enable CORS for $CATALINA_HOME/webapps/geoserver/WEB-INF/web.xml"
+    sed -i "\:</web-app>:i\\
+    <filter>\n\
+      <filter-name>DockerGeoServerCorsFilter</filter-name>\n\
+      <filter-class>org.apache.catalina.filters.CorsFilter</filter-class>\n\
+      <init-param>\n\
+          <param-name>cors.allowed.origins</param-name>\n\
+          <param-value>${GEOSERVER_CORS_ALLOWED_ORIGINS}</param-value>\n\
+      </init-param>\n\
+      <init-param>\n\
+          <param-name>cors.allowed.methods</param-name>\n\
+          <param-value>${GEOSERVER_CORS_ALLOWED_METHODS}</param-value>\n\
+      </init-param>\n\
+      <init-param>\n\
+        <param-name>cors.allowed.headers</param-name>\n\
+        <param-value>${GEOSERVER_CORS_ALLOWED_HEADERS}</param-value>\n\
+      </init-param>\n\
+    </filter>\n\
+    <filter-mapping>\n\
+      <filter-name>DockerGeoServerCorsFilter</filter-name>\n\
+      <url-pattern>/*</url-pattern>\n\
+    </filter-mapping>" "$CATALINA_HOME/webapps/geoserver/WEB-INF/web.xml";
+  fi
+fi
 
 # start tomcat
 exec env JAVA_OPTS="${JAVA_OPTS}" catalina.sh run
