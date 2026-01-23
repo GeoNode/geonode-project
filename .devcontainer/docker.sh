8160e40ba4236352220f187f578fddd4dcaf033d
G.Allegri <giohappy@gmail.com>
2026-01-23 18:04:50 +0100

    Removed Celery dev container

--- .devcontainer/docker.sh
+++ .devcontainer/docker.sh
@@ -2,4 +2,4 @@
 set -o allexport
 source ../.env
 set +o allexport
-docker compose -f ../docker-compose.yml -f docker-compose-dev.yml -f ./django/docker-compose.yml -f ./celery/docker-compose.yml "$@"
+docker compose -f ../docker-compose.yml -f ./docker-compose.yml  "$@"
