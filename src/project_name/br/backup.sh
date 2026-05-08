46747c942c82347f06234852dfc0abb90c1d7e49
gpetrak <gkpetrak@gmail.com>
2026-05-08 17:58:35 +0300

    replace manage.sh with django-admin

--- src/project_name/br/backup.sh
+++ src/project_name/br/backup.sh
@@ -32,7 +32,7 @@ fi
 
 cd /usr/src/{{project_name}}/ 
 
-./manage.sh backup -i -f -c $PWD/{{project_name}}/br/settings_docker.ini --backup-dir /$BKP_FOLDER_NAME/
+django-admin backup -i -f -c $PWD/{{project_name}}/br/settings_docker.ini --backup-dir /$BKP_FOLDER_NAME/
 
 BKP_FILE_LATEST=$(find /$BKP_FOLDER_NAME/*.zip -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1,NR==1 {print $2}')
 BKP_FILE_NAME=$(echo $BKP_FILE_LATEST | tail -n 1 | grep -oP -m 1 "\/$BKP_FOLDER_NAME\/\K.*" | sed 's|.zip||')
